51f268f39dab18a4c56911dbaf83a076c1773762
modxcustomize <modx.customize@gmail.com>
2025-05-08 20:27:18 +0400

    fix: HW4 is completed

--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -1,9 +1,7 @@
 package hw04lrucache
 
 import (
-	"math/rand"
-	"strconv"
-	"sync"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -12,6 +10,23 @@ import (
 func TestCache(t *testing.T) {
 	t.Run("empty cache", func(t *testing.T) {
 		c := NewCache(10)
+
+		if Debug {
+			fmt.Println("\033[H\033[2J")
+			fmt.Println("quee = ", c)
+
+			wasInCache := c.Set("aaa", 100)
+			require.False(t, wasInCache)
+			wasInCache = c.Set("aaa", 101)
+			require.True(t, wasInCache)
+			wasInCache = c.Set("bbb", 777)
+			require.False(t, wasInCache)
+			wasInCache = c.Set("bbb", 778)
+			require.True(t, wasInCache)
+
+			fmt.Println("\n wasInCache = ", wasInCache)
+			fmt.Println("quee after = ", c)
+		}
 
 		_, ok := c.Get("aaa")
 		require.False(t, ok)
@@ -50,30 +65,54 @@ func TestCache(t *testing.T) {
 	})
 
 	t.Run("purge logic", func(t *testing.T) {
-		// Write me
+		c := NewCache(5)
+
+		wasInCache := c.Set("aaa", 100)
+		require.False(t, wasInCache)
+
+		wasInCache = c.Set("bbb", 200)
+		require.False(t, wasInCache)
+
+		val, ok := c.Get("aaa")
+		require.True(t, ok)
+		require.Equal(t, 100, val)
+
+		val, ok = c.Get("bbb")
+		require.True(t, ok)
+		require.Equal(t, 200, val)
+
+		wasInCache = c.Set("aaa", 300)
+		require.True(t, wasInCache)
+
+		c.Clear() // delete all items
+		val, ok = c.Get("aaa")
+		require.False(t, ok)
+		require.Equal(t, nil, val)
 	})
-}
 
-func TestCacheMultithreading(t *testing.T) {
-	t.Skip() // Remove me if task with asterisk completed.
+	/*
+		func TestCacheMultithreading(t *testing.T) {
+			t.Skip() // Remove me if task with asterisk completed.
 
-	c := NewCache(10)
-	wg := &sync.WaitGroup{}
-	wg.Add(2)
+			c := NewCache(10)
+			wg := &sync.WaitGroup{}
+			wg.Add(2)
 
-	go func() {
-		defer wg.Done()
-		for i := 0; i < 1_000_000; i++ {
-			c.Set(Key(strconv.Itoa(i)), i)
+			go func() {
+				defer wg.Done()
+				for i := 0; i < 1_000_000; i++ {
+					c.Set(Key(strconv.Itoa(i)), i)
+				}
+			}()
+
+			go func() {
+				defer wg.Done()
+				for i := 0; i < 1_000_000; i++ {
+					c.Get(Key(strconv.Itoa(rand.Intn(1_000_000))))
+				}
+			}()
+
+			wg.Wait()
 		}
-	}()
-
-	go func() {
-		defer wg.Done()
-		for i := 0; i < 1_000_000; i++ {
-			c.Get(Key(strconv.Itoa(rand.Intn(1_000_000))))
-		}
-	}()
-
-	wg.Wait()
+	*/
 }
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,26 +1,145 @@
 package hw04lrucache
 
+import "fmt"
+
+const Debug = false // mode => go test -run '^TestList$'
 type List interface {
 	Len() int
-	Front() *ListItem
-	Back() *ListItem
+	Front() *ListItem // head
+	Back() *ListItem  // tail
 	PushFront(v interface{}) *ListItem
 	PushBack(v interface{}) *ListItem
 	Remove(i *ListItem)
 	MoveToFront(i *ListItem)
+	PrintAll()
 }
 
-type ListItem struct {
-	Value interface{}
-	Next  *ListItem
+type ListItem struct { // It is simply one entry in the chain..
+	Key   Key         // Used by LRU cache to know which key maps to this item
+	Value interface{} // ..with Value pf any type like struct/int/..
+	Next  *ListItem   // A pointer to the next item in the list.
 	Prev  *ListItem
 }
 
-type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+type list struct { // Doubly Linked List (Primary for list operations).
+	head *ListItem
+	tail *ListItem
+	len  int
 }
 
 func NewList() List {
 	return new(list)
+}
+
+func (l *list) Len() int {
+	return l.len
+}
+
+func (l *list) Front() *ListItem {
+	return l.head
+}
+
+func (l *list) Back() *ListItem {
+	return l.tail
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	newItem := &ListItem{
+		Value: v,
+		Next:  l.head, // point forward to the old head
+	}
+
+	if l.head != nil {
+		l.head.Prev = newItem // item gets as first, and ex-first goes to last
+	} else {
+		l.tail = newItem // first item added is both head and tail
+	}
+
+	l.head = newItem
+	l.len++
+
+	return newItem
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	newItem := &ListItem{
+		Value: v,
+		Prev:  l.tail, // point backward to the old head
+	}
+	if l.tail != nil {
+		l.tail.Next = newItem // item gets as Last, and ex-last goes ...
+	} else {
+		l.head = newItem // first item added is both head and tail
+	}
+
+	l.tail = newItem
+	l.len++
+
+	return newItem
+}
+
+func (l *list) Remove(i *ListItem) {
+	if i == nil {
+		return
+	}
+
+	// If the item has a previous node, update its Next pointer
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	} else {
+		l.head = i.Next
+	}
+
+	// If the item has a next node, update its Prev pointer
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	} else {
+		l.tail = i.Prev
+	}
+
+	l.len--
+
+	// Optionally clear pointers to help GC
+	i.Next = nil
+	i.Prev = nil
+}
+
+func (l *list) PrintAll() {
+	current := l.head
+	for current != nil {
+		fmt.Printf("%v ", current.Value)
+		current = current.Next
+	}
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	if i == nil || l.head == i {
+		return
+	}
+
+	// Unlink i from current position
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	}
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	}
+
+	// If i was tail, update tail
+	if l.tail == i {
+		l.tail = i.Prev
+	}
+
+	// Insert i at front
+	i.Prev = nil
+	i.Next = l.head
+	if l.head != nil {
+		l.head.Prev = i
+	}
+	l.head = i
+
+	// If the list was empty (unlikely here), set tail too
+	if l.tail == nil {
+		l.tail = i
+	}
 }
