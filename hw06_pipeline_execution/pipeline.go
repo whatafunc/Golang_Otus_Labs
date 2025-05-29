90101a3731d7cf18f8bbfc8e57615f7a3535eb6c
modxcustomize <modx.customize@gmail.com>
2025-05-29 13:52:07 +0400

    fix: 2 tests passed, 1 - panics

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,41 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	if len(stages) == 0 { // If no stages, just return the input channel
+		return in
+	}
+
+	current := in // Connect stages in a pipeline
+
+	for _, stage := range stages {
+		// Wrap each stage with cancellation support
+		current = wrapStageWithCancel(stage, done, current)
+	}
+	return current
+}
+
+func wrapStageWithCancel(stage Stage, done In, current In) Out {
+	out := make(Bi)
+
+	go func() {
+		defer close(out)
+		stageOut := stage(current)
+
+		for {
+			select {
+			case <-done: // check before starting to process the next value (func)
+				return
+			case v, ok := <-stageOut:
+				if !ok {
+					return
+				}
+				select {
+				case <-done: // check again but before sending a value to the next stage
+					return
+				case out <- v:
+				}
+			}
+		}
+	}()
+	return out
 }
