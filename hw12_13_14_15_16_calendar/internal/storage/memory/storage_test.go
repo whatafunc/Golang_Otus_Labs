8e73c9cab1253beb1b411bbb78dd7d3ee4ad59b5
modxcustomize <modx.customize@gmail.com>
2025-07-28 17:54:16 +0400

    fix: CI timeout

--- hw12_13_14_15_16_calendar/internal/storage/memory/storage_test.go
+++ hw12_13_14_15_16_calendar/internal/storage/memory/storage_test.go
@@ -73,7 +73,7 @@ func TestStorage_Timeout(t *testing.T) {
 	s := New()
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Microsecond)
 	defer cancel()
-	time.Sleep(2 * time.Microsecond) // Ensure timeout expires
+	time.Sleep(20 * time.Microsecond) // Ensure timeout expires
 
 	_, err := s.GetEvent(ctx, 1)
 	if !errors.Is(err, ErrContextCancel) {
