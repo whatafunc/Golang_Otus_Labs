4842747c18df514227c031975bbd216ad7f431bf
modxcustomize <modx.customize@gmail.com>
2025-08-25 19:54:44 +0400

    feat: added tests

---
+++ hw12_13_14_15_16_calendar/internal/server/grpc/server_test.go
@@ -0,0 +1,22 @@
+package calendarGRPC
+
+import (
+	"context"
+	"testing"
+
+	calendarpb "github.com/whatafunc/Golang_Otus_Labs/hw12_13_14_15_16_calendar/calendarGRPC/pb"
+)
+
+func TestAlwaysPass(t *testing.T) {
+	// This will always succeed
+}
+
+func TestCreateEventReturnsErrorWhenAppIsNil(t *testing.T) {
+	server := &EventServer{application: nil}
+	req := &calendarpb.CreateEventRequest{Event: &calendarpb.Event{}}
+
+	_, err := server.CreateEvent(context.Background(), req)
+	if err == nil {
+		t.Fatal("expected error when application is nil, got nil")
+	}
+}
